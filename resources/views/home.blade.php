a658e124bd5bd9fdf6d6e278f195832ac34adb4e
Cosphi84 <risam1984@gmail.com>
2023-01-16 16:29:18 +0700

    sdada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,25 +18,11 @@
               <div class="form-outline mb-4">
                 <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
                 <label class="form-label" for="typePasswordX-2">Password</label>
-              </div>
-  
-              <!-- Checkbox -->
-              <div class="form-check d-flex justify-content-start mb-4">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="form1Example3"
-                />
-                <label class="form-check-label" for="form1Example3"> Remember password </label>
-              </div>
+              </div>              
   
               <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
   
               <hr class="my-4">
-  
-              <button class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;" type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
-              <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;" type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
   
             </div>
           </div>
